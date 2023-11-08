00e01b79ac37122ce96c848ef1e775f6897e13fc
gimonchik <fedorgorlenko@gmail.com>
2023-11-08 13:05:55 +0300

    added clear_storage(), prepared speed up function

--- main.py
+++ main.py
@@ -11,9 +11,22 @@ bot_link = hlink("@proton_equalizer_bot", "https://t.me/proton_equalizer_bot")
 mode = ParseMode.HTML
 
 
+def clear_storage(chat_id):
+    for file in os.listdir(f"storage/{chat_id}"):
+        os.remove(f"storage/{chat_id}/{file}")
+
+
+async def request_processing(message, chat_id):
+    await bot.delete_message(chat_id=chat_id, message_id=message.message_id)
+    await message.answer("processing...")
+
+
 async def reply_to_user(kb, message, valid):
     if valid:
-        await message.answer(f"what would you like me to do?", reply_markup=kb)
+        await message.answer(
+            f"what would you like me to do?",
+            reply_markup=kb,
+        )
     else:
         await message.answer("hold on, that's not an audio")
 
@@ -42,16 +55,29 @@ async def voice_processing(message: types.Message):
     content = message.content_type
     kb = InlineKeyboardMarkup()
     valid = False
-    print(content)
     if content == "audio":
         valid = True
         chat_id = message["chat"]["id"]
-        path = f"storage/{chat_id}/test.mp3"
+        source_path = f"storage/{chat_id}/source.mp3"
         btn_1 = InlineKeyboardButton("speed up", callback_data="speed")
         btn_2 = InlineKeyboardButton("slow down", callback_data="slow")
         kb.add(btn_1, btn_2)
-        await message.audio.download(destination_file=path)
+        await message.audio.download(destination_file=source_path)
     await reply_to_user(kb, message, valid)
+
+
+@dp.callback_query_handler(text=["speed"])
+async def speed_up(callback_query: types.CallbackQuery):
+    chat_id = callback_query.message['chat']['id']
+    await request_processing(callback_query.message, chat_id)
+    source_path = f"storage/{chat_id}/source.mp3"
+    result_path = f"storage/{chat_id}/result.mp3"
+    await callback_query.message.answer_document(
+        InputFile(result_path),
+        caption=bot_link,
+        parse_mode=mode,
+    )
+    clear_storage(chat_id)
 
 
 if __name__ == "__main__":
